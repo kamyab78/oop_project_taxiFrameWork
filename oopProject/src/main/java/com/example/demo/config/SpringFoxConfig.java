bf8dc32ef56917b734c9b3879651af094fe15dbe
kamyab78 <amirhosseinghassemi78@yahoo.com>
2021-02-05 15:07:43 +0330

    Add Config swagger

---
+++ oopProject/src/main/java/com/example/demo/config/SpringFoxConfig.java
@@ -0,0 +1,78 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+//@Import(BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfig {
+
+    Contact contact = new Contact(
+            "",
+            "",
+            "");
+    //
+    List<VendorExtension> vext = new ArrayList<>();
+    ApiInfo apiInfo = new ApiInfo(
+            "reservation- Backend API",
+            "This is the best stuff since sliced bread - API",
+            "1.0.0",
+            "",
+            contact,
+            "MIT",
+            "",
+            vext);
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+
+    @Bean
+    public Docket actuatorApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("reservation")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo)
+                .securityContexts(Arrays.asList(actuatorSecurityContext()))
+                .securitySchemes(Arrays.asList(basicAuthScheme()));
+    }
+
+    private SecurityContext actuatorSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .forPaths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+}
